bf0bfa06ae05da7bfc7fe1e8a8c2613b45fc8ebe
Unleavened Code <Unleavened.Code@gmail.com>
2020-02-20 16:35:00 +0300

    Initial Commit

--- PerCal.py
+++ PerCal.py
@@ -1,25 +1,15 @@
+"""
+@param grid: an MxN array of integers
+@param point: (x,y) tuple where x is the coordinate which corresponds to the row and y corresponds to the column
+@return: perimeter
+
+"""
+
+grid = [[0, 1, 0, 0], [2,1,0,1], [0, 1, 1, 0], [1,0,0,3]] # this is a 2D grid
+point = [2,] # this point is a reference key pair, [x,y] x is the index of the row, y ios the index of the column
+
 def calculatePerimeter(grid, point):
-   """
-   Given an MxN grid of integers and a point in the 0-based grid,
-   calculate the perimeter of the largest island to which that point belongs.
-   An island is a group of horizontally and vertically contiguous cells with the same value.
-   Example:
-   Grid:
-   ---�*****���-----
-   | 0 * 1 * 0 | 0 |
-   �---*�--*���-----
-   | 2 * 1 * 0 | 1 |
-   �---*--�*****�---
-   | 0 * 1 | 1 * 0 |
-   �---*********�---
-   | 1 | 0 | 0 | 3 |
-   ������-----------
-   Given point: (1, 1)
-   The perimeter here is denoted in *'s, and would be 10.
-   @param grid: an MxN array of integers
-   @param point: (x,y) tuple where x is the coordinate which corresponds to the row and y corresponds to the column
-   @return: perimeter
-   """
-   perimeter = None
+   perimeter = 0
    # Your code goes here
+   
    return perimeter
